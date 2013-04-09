427e392e6675cc7d2b6be64f1d62037e317cf54c
ianbattersby <ian.battersby@gmail.com>
2013-04-09 07:02:55 +0100

    Moved to rake builds.

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Runtime.CompilerServices;
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyCompany("Mark Rendle")]
+[assembly: AssemblyProduct("Flux")]
+[assembly: AssemblyCopyright("Copyright (C) Mark Rendle 2010-2013")]
+[assembly: AssemblyTrademark("892b3c26231f03a53623b60846b91d358e46ec02")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.3.3.50707")]
+[assembly: AssemblyFileVersion("0.3.3.50707")]
+
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyInformationalVersion("0.3.3.50707")]
+
+[assembly: InternalsVisibleTo("Flux.Test.SimpleWeb")]
+[assembly: InternalsVisibleTo("Flux.Test")]
